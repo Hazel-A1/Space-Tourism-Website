e1efbd7af6e5b1ec1197ccca13cd4ddde280e5ba
Akanimo <Akanumoh4@gmail.com>
2023-04-21 23:53:04 +0100

    Home

---
+++ codes/index.js
@@ -0,0 +1,7 @@
+document.getElementById("open").onclick = function() {
+    document.getElementById("dropdown_ctn").style.display = "block";
+}
+
+document.getElementById("close").onclick = function() {
+    document.getElementById("dropdown_ctn").style.display = "none";
+}
