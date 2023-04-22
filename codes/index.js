cc321a7448e6f738982e4a7bf04994825d682ca5
Akanimo <Akanumoh4@gmail.com>
2023-04-22 23:28:40 +0100

    Home

--- codes/index.js
+++ codes/index.js
@@ -1,7 +1,39 @@
 document.getElementById("open").onclick = function() {
-    document.getElementById("dropdown_ctn").style.display = "block";
+    document.getElementById("dropdown_ctn").style.right = "0";
 }
 
 document.getElementById("close").onclick = function() {
-    document.getElementById("dropdown_ctn").style.display = "none";
+    document.getElementById("dropdown_ctn").style.right = "-100%";
 }
+
+$(function() {
+    $("#navbar").load("navbar.html");
+  });
+  
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('name').textContent = data.name;
+    document.getElementById('age').textContent = data.age;
+    document.getElementById('email').textContent = data.email;
+  })
+  .catch(error => console.error(error));
+
+
+function openCity(evt) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+    
